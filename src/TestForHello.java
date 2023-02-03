a294a5fb1ea613219eb9c51986d0b15300c73857
MAwei-MV <15717325053@163.com>
2023-02-03 21:44:37 +0800

    123

--- src/TestForHello.java
+++ src/TestForHello.java
@@ -1,5 +1,8 @@
 public class TestForHello {
     public static void main(String args[]) {
         System.out.println("hello world");
+        System.out.print("in main");
+        System.out.println("in cache");
+        System.out.println("in workSpace");
     }
 }
