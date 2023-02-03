664be55b655c2682611889abd8c0461a574def5b
MAwei-MV <15717325053@163.com>
2023-02-03 22:43:39 +0800

    2

--- src/TestForHello.java
+++ src/TestForHello.java
@@ -2,5 +2,6 @@ public class TestForHello {
     public static void main(String args[]) {
         System.out.println("hello world");
         System.out.println("in develop1");
+        System.out.println("in develop commit 1");
     }
 }
