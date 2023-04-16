55e9a4da7ca290a7c1bf8c16e33c1a8684295462
MAwei-MV <15717325053@163.com>
2023-04-16 19:14:23 +0800

    third commit

--- src/WorkInMain.java
+++ src/WorkInMain.java
@@ -2,5 +2,6 @@ public class WorkInMain {
     public static void main(String args[]) {
         System.out.println("I am first commit");
         System.out.println("I am second commit");
+        System.out.println("I am third commit");
     }
 }
